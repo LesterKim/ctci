192d8bdd9961998d849e275acc75ff73d230a6c7
Lester <lkim@post.harvard.edu>
2019-04-19 18:35:25 -0400

    feat(coin_changes.py): Added alternative solution

--- exercises/coin_changes.py
+++ exercises/coin_changes.py
@@ -15,4 +15,14 @@ def make_change(n, denoms):
 
 	return count
 
-print(make_change(100, denoms))
+def make_change2(n, denoms):
+	if n < 5: return 1
+
+	if len(denoms) == 1: return 1
+
+	for i, denom in enumerate(denoms):
+		if n >= denom:
+			return make_change2(n-denom, denoms) + make_change2(n, denoms[i+1:])
+
+assert make_change(100, denoms) == 293
+assert make_change2(100, denoms) == 293
