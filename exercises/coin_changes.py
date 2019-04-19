8fedace7569d391ecbf2c8d8f3f2dafd6baede1d
Lester <lkim@post.harvard.edu>
2019-04-19 18:49:39 -0400

    fix(coin_changes.py): Added bug fixes

--- exercises/coin_changes.py
+++ exercises/coin_changes.py
@@ -16,13 +16,19 @@ def make_change(n, denoms):
 	return count
 
 def make_change2(n, denoms):
-	if n < 5: return 1
+	if n == 0: return 1
 
 	if len(denoms) == 1: return 1
 
 	for i, denom in enumerate(denoms):
 		if n >= denom:
-			return make_change2(n-denom, denoms) + make_change2(n, denoms[i+1:])
+			count = make_change2(n - denom, denoms[i:])
+
+			if i < len(denoms) - 1:
+				count += make_change2(n, denoms[i+1:])
+
+			return count
 
 assert make_change(100, denoms) == 293
+assert make_change2(4, denoms) == 1
 assert make_change2(100, denoms) == 293
